ef5858bec47d9dc8134ef5bea7c4d4ac2c641a0b
Johan Johansson <96.bryal@gmail.com>
2018-05-09 14:48:00 +0200

    save screensize onready for paddle

--- paddle.gd
+++ paddle.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 const MOVE_SPEED = 260
+onready var screensize = get_viewport_rect().size
 
 func height():
     return get_node("collision").shape.extents.y * 2
@@ -17,5 +18,5 @@ func _process(delta):
     # move up and down based on input
     if Input.is_action_pressed(which+"_move_up") and top() > 0:
         position.y -= MOVE_SPEED * delta
-    if Input.is_action_pressed(which+"_move_down") and bottom() < get_viewport_rect().size.y:
+    if Input.is_action_pressed(which+"_move_down") and bottom() < screensize.y:
         position.y += MOVE_SPEED * delta
